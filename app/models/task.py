7e126d58228c57dbaf6ab45da2c3ed87cb502f62
Sarah Koch <sarahkoch810@gmail.com>
2024-10-31 14:35:02 -0700

    Created class method to create a dict of attributes.

--- app/models/task.py
+++ app/models/task.py
@@ -1,9 +1,18 @@
 from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from constants import ID, NAME, DESCRIPTION, COMPLETED_AT
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=True)
     completed_at: Mapped[datetime] = mapped_column(nullable=True)
+
+    def to_dict(self):
+        return {
+            ID: self.id, 
+            NAME: self.name,
+            DESCRIPTION: self.description,
+            COMPLETED_AT: self.completed_at
+        }
