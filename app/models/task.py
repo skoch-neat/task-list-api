2ad8b7c1667b75d213d0b6475e5181188cf1ff3b
Sarah Koch <sarahkoch810@gmail.com>
2024-10-31 14:57:52 -0700

    Constants updated. Requirements updated to include pytest-cov. Task model modified to include is_complete and completed_at, not one or the other. Commented out skip decorators in wave 1 tests not requiring additional code.

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+from sqlalchemy import DateTime
 from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
 from ..db import db
 from constants import ID, TITLE, DESCRIPTION, IS_COMPLETE
 
@@ -7,12 +9,15 @@ class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=True)
-    is_complete: Mapped[datetime] = mapped_column(nullable=True)
+    completed_at: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True, default=None)
+
+    def is_complete(self):
+        return self.completed_at is not None
 
     def to_dict(self):
         return {
             ID: self.id, 
             TITLE: self.title,
             DESCRIPTION: self.description,
-            IS_COMPLETE: self.is_complete
+            IS_COMPLETE: self.is_complete()
         }
