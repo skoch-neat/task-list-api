ef3dc6184d427ef759f1ae3c79ce2dfd462b4cf8
Sarah Koch <sarahkoch810@gmail.com>
2024-10-31 14:33:54 -0700

    Corrected constants list. Replaced name with title.

--- constants.py
+++ constants.py
@@ -3,7 +3,9 @@ Constants for the Task List API
 '''
 
 # Keys
-ID = "id"
-NAME = "name"
-DESCRIPTION = "description"
-COMPLETED_AT = "completed_at"
+ID = 'id'
+TITLE = 'title'
+DESCRIPTION = 'description'
+COMPLETED_AT = 'completed_at'
+ORDER_BY = 'order_by'
+MESSAGE = 'message'
