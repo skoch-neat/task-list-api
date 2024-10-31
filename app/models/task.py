37390a058be0173aac3592788a8b43f17c1cb3bb
Sarah Koch <sarahkoch810@gmail.com>
2024-10-31 14:03:58 -0700

    Added attributes to Task model

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,9 @@
+from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=True)
+    completed_at: Mapped[datetime] = mapped_column(nullable=True)
