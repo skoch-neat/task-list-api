5efb26fe89c5accd96db1aae82117d34ba92f0bc
Sarah Koch <sarahkoch810@gmail.com>
2024-10-31 14:43:15 -0700

    Corrected attribute 'completed_at' to 'is_complete'

--- app/models/task.py
+++ app/models/task.py
@@ -1,18 +1,18 @@
 from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
-from constants import ID, TITLE, DESCRIPTION, COMPLETED_AT
+from constants import ID, TITLE, DESCRIPTION, IS_COMPLETE
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=True)
-    completed_at: Mapped[datetime] = mapped_column(nullable=True)
+    is_complete: Mapped[datetime] = mapped_column(nullable=True)
 
     def to_dict(self):
         return {
             ID: self.id, 
             TITLE: self.title,
             DESCRIPTION: self.description,
-            COMPLETED_AT: self.completed_at
+            IS_COMPLETE: self.is_complete
         }
--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@ Constants for the Task List API
 ID = 'id'
 TITLE = 'title'
 DESCRIPTION = 'description'
-COMPLETED_AT = 'completed_at'
+IS_COMPLETE = 'is_complete'
 ORDER_BY = 'order_by'
 MESSAGE = 'message'
 TASK = 'task'
