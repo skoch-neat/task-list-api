039a2993669f9797b42cbe50b3c972b85574e2cc
Sarah Koch <sarahkoch810@gmail.com>
2024-11-02 12:06:34 -0700

    Identified and implemented a one-to-many relationship between Goal and Task models. Created instance and class methods for converting Goal objects to and from dictionaries. Created class method to convert Task object from dictionary. Migrated and ran Goal and Task model changes.

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,6 +1,21 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .task import Task
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..db import db
+from constants import ID, TITLE
 
 class Goal(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(nullable=False)
+    tasks: Mapped[list['Task']] = relationship(back_populates='goal')
+
+    def to_dict(self):
+        return {
+            ID: self.id, 
+            TITLE: self.title
+        }
+
+    @classmethod
+    def from_dict(cls, goal_data):
+        return cls(title=goal_data[TITLE])
--- app/models/task.py
+++ app/models/task.py
@@ -1,15 +1,20 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .goal import Goal
 from datetime import datetime
-from sqlalchemy import DateTime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import Optional
 from ..db import db
-from constants import ID, TITLE, DESCRIPTION, IS_COMPLETE
+from constants import ID, TITLE, DESCRIPTION, IS_COMPLETE, COMPLETED_AT
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=True)
-    completed_at: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True, default=None)
+    completed_at: Mapped[Optional[datetime]] = mapped_column(nullable=True, default=None)
+    goal_id: Mapped[Optional[int]] = mapped_column(ForeignKey('goal.id'))
+    goal: Mapped[Optional['Goal']] = relationship(back_populates='tasks')
 
     def is_complete(self):
         return self.completed_at is not None
@@ -21,3 +26,11 @@ class Task(db.Model):
             DESCRIPTION: self.description,
             IS_COMPLETE: self.is_complete()
         }
+    
+    @classmethod
+    def from_dict(cls, task_data):
+        return cls(
+            title=task_data[TITLE],
+            description=task_data[DESCRIPTION],
+            completed_at=task_data.get(COMPLETED_AT)
+        )     
