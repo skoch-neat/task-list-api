dc6e5e70a2b88134c9f793503847ba1d7374d9ab
Sarah Koch <sarahkoch810@gmail.com>
2024-11-06 14:47:59 -0800

    Refactored Goal routes: organized by CRUD task, modified individual routes to maintain consistent logic and patterns, updated import statements based on use.

--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, request
 
 from app.models.task import Task
 
 from ..db import db
 from ..models.goal import Goal
-from constants import TITLE, GOAL, DETAILS
+from constants import DETAILS, GOAL, ID, TASK_IDS, TASKS, TITLE
 from .route_utilities import create_model, get_models_with_filters, validate_model
 
 bp = Blueprint('goals_bp', __name__, url_prefix='/goals')
@@ -14,6 +14,22 @@ def create_goal():
     request_body = request.get_json()
     return create_model(Goal, request_body)
 
+@bp.post('/<goal_id>/tasks')
+def create_task_with_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+    request_body = request.get_json()
+
+    task_ids = request_body.get(TASK_IDS, [])
+    tasks = [validate_model(Task, task_id) for task_id in task_ids]
+    goal.tasks.extend(tasks)
+
+    db.session.commit()
+
+    return {
+        ID: goal.id,
+        TASK_IDS: task_ids
+    }
+
 @bp.get('')
 def get_all_goals():
     return get_models_with_filters(Goal, request.args)
@@ -21,6 +37,16 @@ def get_all_goals():
 @bp.get('/<goal_id>')
 def get_one_goal(goal_id):
     return {GOAL: validate_model(Goal, (goal_id)).to_dict()}
+
+@bp.get('/<goal_id>/tasks')
+def get_tasks_by_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+
+    return {
+        ID: goal.id,
+        TITLE: goal.title,
+        TASKS: [task.to_dict() for task in goal.tasks]
+    }
 
 @bp.put('/<goal_id>')
 def update_goal(goal_id):
@@ -41,40 +67,3 @@ def delete_goal(goal_id):
     db.session.commit()
 
     return {DETAILS: f'Goal {goal_id} "{goal.title}" successfully deleted'}
-
-@bp.get('/<goal_id>/tasks')
-def get_tasks_by_goal(goal_id):
-    goal = validate_model(Goal, goal_id)
-    return {
-        'id': goal.id,
-        'title': goal.title,
-        'tasks': [task.to_dict() for task in goal.tasks]
-    }
-    
-@bp.post('/<goal_id>/tasks')
-def create_task_with_goal(goal_id):
-    goal = validate_model(Goal, goal_id)
-    
-    request_body = request.get_json()
-
-    if 'task_ids' not in request_body:
-        abort(make_response({'details': 'Missing task_ids'}, 400))
-    
-    task_ids = request_body['task_ids']
-
-    if not isinstance(task_ids, list):
-        abort(make_response({'details': 'task_ids must be a list'}), 400)
-    
-    tasks = []
-    for task_id in task_ids:
-        task = validate_model(Task, task_id)
-        task.goal_id = goal.id
-        tasks.append(task)
-
-    db.session.add_all(tasks)
-    db.session.commit()
-
-    return make_response({
-        'id': goal.id,
-        'task_ids': task_ids
-    }, 200)
